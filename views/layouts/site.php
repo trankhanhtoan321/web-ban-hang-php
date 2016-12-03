e3f1e8ba65510f33b0dab3ffb752a78bb416c00c
Trần Khánh Toàn <trankhanhtoan321@gmail.com>
2016-12-03 09:10:44 +0700

    upload code web at the first time

---
+++ views/layouts/site.php
@@ -0,0 +1,30 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<!-- bootstrap -->
+	<link rel="stylesheet" type="text/css" href="/style/bootstrap/css/bootstrap.min.css" />
+	<script src="/style/bootstrap/js/bootstrap.min.js"></script>
+	<script src="/js/jquery.min.js"></script>
+	<!-- ckeditor -->
+	<script src="/lib/ckeditor/ckeditor.js"></script>
+	<!-- nivo slider -->
+	<script src="/lib/nivoslider/jquery.nivo.slider.js"></script>
+	<link rel="stylesheet" type="text/css" href="/lib/nivoslider/nivo-slider.css" />
+	<link rel="stylesheet" type="text/css" href="/lib/nivoslider/themes/default/default.css" />
+	<!-- custom css -->
+	<link rel="stylesheet" type="text/css" href="/style/style.css" />
+	<!-- SEO -->
+	<title><?php if(isset($SEO['title'])) echo $SEO['title']; ?></title>
+	<meta name="description" content="<?php if(isset($SEO['description'])) echo $SEO['description']; ?>"/>
+	<meta name="keywords" content="<?php if(isset($SEO['keywords'])) echo $SEO['keywords']; ?>" />
+	<meta name="author" content="<?php if(isset($SEO['keywords'])) echo $SEO['keywords']; ?>" />
+</head>
+<body>
+	
+</body>
+</html>
